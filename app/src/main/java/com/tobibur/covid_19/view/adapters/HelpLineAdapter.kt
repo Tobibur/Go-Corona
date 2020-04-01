132a1a39aa593d352651c509fad0322292c4c9ce
sahil <sahil.khan@eduaid.co.in>
2020-04-01 13:15:49 +0530

    phone numbers item improved

--- app/src/main/java/com/tobibur/covid_19/view/adapters/HelpLineAdapter.kt
+++ app/src/main/java/com/tobibur/covid_19/view/adapters/HelpLineAdapter.kt
@@ -6,7 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.tobibur.covid_19.R
 import com.tobibur.covid_19.model.Help
 import com.tobibur.covid_19.utils.inflate
-import kotlinx.android.synthetic.main.stats_view_item.view.*
+import kotlinx.android.synthetic.main.item_phone_numbers.view.*
 
 class HelpLineAdapter(private val helplines: List<Help>) :
     RecyclerView.Adapter<HelpLineAdapter.HelpViewHolder>() {
@@ -14,7 +14,7 @@ class HelpLineAdapter(private val helplines: List<Help>) :
     inner class HelpViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HelpViewHolder {
-        return HelpViewHolder(parent.inflate(R.layout.stats_view_item))
+        return HelpViewHolder(parent.inflate(R.layout.item_phone_numbers))
     }
 
     override fun getItemCount(): Int {
@@ -24,8 +24,8 @@ class HelpLineAdapter(private val helplines: List<Help>) :
     override fun onBindViewHolder(holder: HelpViewHolder, position: Int) {
         val helpline = helplines[position]
         holder.itemView.apply {
-            txtCountryName.text = helpline.place
-            txtCases.text = helpline.helpline
+            phoneName.text = helpline.place
+            phoneNumber.text = helpline.helpline
         }
     }
 }
